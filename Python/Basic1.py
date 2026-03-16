c11d3dfce759f7d73cd0551a6711c204bd2dda30
Ullas-N-S <ullasn464@gmail.com>
2026-03-16 22:17:54 +0530

    Learning_day2

---
+++ Python/Basic1.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+print("Hello,", name)
+
+List = [1,2,3,4,5,6]
+Sec_list = [1,2.45,'great']
+Tuple = (1,2.456,'Age')
+Dictionary = {'Name':name, 'Age':21, 'Sem':6, 'Percentage':89.45}
+set_values = {3, 4.5, 'None','Done', 3.45}
+none_var=None
+
+print(Tuple)
+print(sum(List))
+print(sum(range(1,6)))
+print(isinstance('Hello',int))
+print('Name',Dictionary['Name'])
+print(set_values)
+print(type(none_var))
